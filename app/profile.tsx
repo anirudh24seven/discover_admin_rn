4c93c2b642f8f7ee9f706d50f3980863fa985f2d
Anirudh S <anirudh24seven@gmail.com>
2025-05-10 22:20:18 +0530

    Client side check-in functionality

--- app/profile.tsx
+++ app/profile.tsx
@@ -15,10 +15,18 @@ import {
 import { Text } from '~/components/ui/text';
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
 import { useLocalSearchParams } from 'expo-router';
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: 'http://35.154.49.171:4000/api',
+  headers: {
+    Accept: 'application/json',
+  }
+})
 
 export default function Profile() {
   const { stringUser } = useLocalSearchParams();
-  const user = JSON.parse(stringUser);
+  const [user, setUser] = React.useState<User | null>(() => JSON.parse(stringUser));
   console.log("Profile user", user)
 
   type User = {
@@ -51,76 +59,54 @@ export default function Profile() {
       checked_in: string
     }
 
+  const buttonPressed = () => {
+    console.log("check-in buttonPressed", user.qr)
+
+    const updatedUser = {
+      ...user,
+      checked_in: true, 
+    }
+
+    setUser(updatedUser)
+    
+    // api.get(`/check-in/${qr}`)
+    //   .then(response => {
+    //     console.log("response", response);
+    //     const fetchedUser = response.data.user
+    //     if (fetchedUser) {
+    //       console.log("user: ", fetchedUser)
+    //       setUser(fetchedUser)
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.error("error", error);
+    //   })
+  }
+
   return (
     (user &&
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Card className={`w-full max-w-sm p-6 rounded-2xl ${user.checked_in === "y" ? "border-4 border-green-500" : ""}`}>
-        <CardHeader className='items-center'>
-          <Avatar alt="Rick Sanchez's Avatar" className='w-24 h-24'>
-            <AvatarFallback>
-              <Text className="text-2xl">{user.division}</Text>
+    <View className='flex-1 justify-center items-center gap-5 px-6 bg-secondary/30'>
+      <Card className={`w-full max-w-sm mx-6 rounded-2xl ${user.checked_in === true ? "border-4 border-green-500" : ""}`}>
+        <CardHeader className='items-center bg-primary rounded-t-xl'>
+          <Text className='text-primary-foreground'>Division</Text>
+          <Avatar alt="Avatar" className='w-24 h-24'>
+            <AvatarFallback className="bg-secondary">
+              <Text className="text-2xl text-2xl font-bold text-secondary-foreground">{user.division}</Text>
             </AvatarFallback>
           </Avatar>
           <View className='p-3' />
-          <CardTitle className='text-center'>{user.name_to_be_displayed_in_id_card}</CardTitle>
+          <CardTitle className='text-center text-primary-foreground'>{user.name_to_be_displayed_in_id_card}</CardTitle>
+          <Text className='text-xl font-semibold text-center mt-2 text-primary-foreground'>{user.club_name}</Text>
         </CardHeader>
         <CardContent>
-          <View className='flex-row justify-around gap-3 mb-4'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Club</Text>
-              <Text className='text-xl font-semibold text-center'>{user.club_name}</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Mobile</Text>
-              <Text className='text-xl font-semibold'>{user.mobile}</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Email</Text>
-              <Text className='text-xl font-semibold'>{user.email}</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Member ID</Text>
-              <Text className='text-xl font-semibold'>{user.member_id}</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Division</Text>
-              <Text className='text-xl font-semibold'>{user.division}</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
+          <View className='flex-row justify-around gap-3 mt-4'>
             <View className='items-center'>
               <Text className='text-sm text-muted-foreground'>Ticket type</Text>
               <Text className='text-xl font-semibold'>{user.ticket_type}</Text>
             </View>
             <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Food preference</Text>
-              <Text className='text-xl font-semibold'>{user.food_preference}</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Amount paid</Text>
-              <Text className='text-xl font-semibold'>{user.amount_paid}</Text>
-            </View>
-            <View className='items-center'>
               <Text className='text-sm text-muted-foreground'>Payment verified</Text>
-              <Text className='text-xl font-semibold'>{user.payment_verified}</Text>
+              <Text className='text-xl font-semibold'>{user.payment_verified === 'y' ? '✅' : '❌'}</Text>
             </View>
           </View>
         </CardContent>
@@ -128,20 +114,24 @@ export default function Profile() {
           <View className='flex-row justify-around gap-3'>
             <View className='items-center'>
               <Text className='text-sm text-muted-foreground'>Checked-in</Text>
-              <Text className='text-xl font-semibold'>{user.checked_in}</Text>
+              <Text className={`text-xl font-semibold ${user.checked_in ? "text-green-500" : ""}`}>
+                {user.checked_in === true ? 'Yes' : 'No'}
+              </Text>
             </View>
           </View>
         </CardContent>
-        <CardFooter className='flex-col gap-3 pb-0'>
-          <View />
-          <Button
-            variant='outline'
-            className='shadow shadow-foreground/5'
-            // onPress={updateProgressValue}
-          >
-            <Text>Check-in</Text>
-          </Button>
-        </CardFooter>
+        {user.checked_in ? <></> :
+          <CardFooter className='flex-col gap-3 pb-4'>
+            <View />
+            <Button
+              variant='outline'
+              className='shadow shadow-foreground/5'
+              onPress={buttonPressed}
+            >
+              <Text>Check-in</Text>
+            </Button>
+          </CardFooter>
+        }
       </Card>
     </View>
   )
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,16 @@
+const path = require('path')
+
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enablePackageExports = false;
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  zustand: path.resolve(__dirname, 'node_modules/zustand/traditional'),
+}
+
 
 module.exports = withNativeWind(config, { input: './global.css' });
